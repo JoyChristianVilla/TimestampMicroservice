1eac46ac8dde0cdb55e72b86ec0741ff45e1358a
Christian Villa <joychristianvilla@gmail.com>
2017-06-30 14:03:40 -0700

    configure for deployment

--- program.js
+++ program.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var url = require('url');
 var obj;
 var givenDate;
 var json;
@@ -9,30 +8,35 @@ var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 app.get('/:date', function(req, res) {
   //Create a new date object called givenDate based on the date we received in the url
   givenDate = new Date(req.params.date);
+  //Create an object with the unix and natural time based on givenDate
   obj = {
-    'unix': givenDate.getTime(),
-    'natural': months[givenDate.getMonth()] + ' ' + givenDate.getDate() + ', ' + givenDate.getFullYear()
+    unix: givenDate.getTime(),
+    natural: months[givenDate.getMonth()] + ' ' + givenDate.getDate() + ', ' + givenDate.getFullYear()
   };
-
+  //If unix property has a falsey value, check if the date parameter given is a number
   if (!obj['unix']) {
+    //If the date parameter is not a number, then make the unix property and the natural property have a value of null
     if (isNaN(Number(req.params.date))) {
       obj['unix'] = null;
       obj['natural'] = null;
+    //If the date parameter is a number, then reassign givenDate with the unix timestamp given, turning it from a string into a number so that it works
     } else {
       givenDate = new Date(Number(req.params.date));
+    //Then reassign obj based on the new value of givenDate
       obj = {
-        'unix': givenDate.getTime(),
-        'natural': months[givenDate.getMonth()] + ' ' + givenDate.getDate() + ', ' + givenDate.getFullYear()
+        unix: givenDate.getTime(),
+        natural: months[givenDate.getMonth()] + ' ' + givenDate.getDate() + ', ' + givenDate.getFullYear()
       };
     }
   }
 
   //Header for the response
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  //Turn json into a string
-  json = JSON.stringify(obj);
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // //Turn json into a string
+  // json = JSON.stringify(obj);
   //Send the stringified json as the response to the client
-  res.end(json);
+  res.send(obj);
 });
-app.listen(3000);
-console.log('Congregation is running on port 3000');
+app.listen(3000, function() {
+  console.log('Congregation is running on port 3000');
+});
